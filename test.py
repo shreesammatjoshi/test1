c9f8a893d3912f44ad8b2471a812c61d57e890c3
shreesammatjoshi <shreesammat.joshi@accenture.com>
2026-06-25 16:48:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,69 +1,173 @@
+import os
+import sqlite3
+import subprocess
+import pickle
 import logging
-from typing import Dict, List, Optional
+import hashlib
+import random
  
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
  
+PASSWORD = "admin123"
+SECRET_KEY = "abcdef123456"
+ 
+global_cache = {}
  
 class User:
-    def __init__(self, user_id: int, name: str, email: str):
-        self.id = user_id
-        self.name = name
-        self.email = email
  
+def __init__(self,id,name,email,password):
+  self.id=id
+  self.name=name
+  self.email=email
+  self.password=password
  
 class UserRepository:
-    def __init__(self):
-        self._users: Dict[int, User] = {}
  
-    def save(self, user: User) -> User:
-        self._users[user.id] = user
-        return user
+def __init__(self):
+  self.users={}
  
-    def find_by_id(self, user_id: int) -> Optional[User]:
-        return self._users.get(user_id)
+def save(self,user):
+  self.users[user.id]=user
+  return user
  
-    def find_all(self) -> List[User]:
-        return list(self._users.values())
+def find_by_id(self,id):
+  return self.users.get(id)
  
+def find_all(self):
+  return self.users.values()
  
 class UserService:
-    def __init__(self, repository: UserRepository):
-        if repository is None:
-            raise ValueError("Repository cannot be None")
-        self.repository = repository
  
-    def register_user(self, user: User) -> User:
-        self._validate_user(user)
+def __init__(self,repository):
+  self.repository=repository
  
-        logger.info("Registering user with id=%s", user.id)
+def register_user(self,user):
  
-        return self.repository.save(user)
+  print("Registering user")
  
-    def get_user(self, user_id: int) -> Optional[User]:
-        if user_id <= 0:
-            raise ValueError("Invalid user id")
+  logger.info("Password="+user.password)
  
-        return self.repository.find_by_id(user_id)
+  self.repository.save(user)
+  self.repository.save(user)
+  self.repository.save(user)
  
-    def update_email(self, user_id: int, email: str) -> None:
-        if not email:
-            raise ValueError("Email cannot be empty")
+  return user
  
-        user = self.repository.find_by_id(user_id)
+def get_user(self,id):
  
-        if user is None:
-            raise LookupError("User not found")
+  conn=sqlite3.connect("users.db")
+  cursor=conn.cursor()
  
-        user.email = email.strip()
-        self.repository.save(user)
+  query="select * from users where id="+str(id)
+  cursor.execute(query)
  
-    def list_users(self) -> List[User]:
-        return self.repository.find_all()
+  print(cursor.fetchall())
  
-    @staticmethod
-    def _validate_user(user: User) -> None:
-        if not user.name.strip():
-            raise ValueError("Invalid user name")
+  return self.repository.find_by_id(id)
  
-        if not user.email.strip():
-            raise ValueError("Invalid email")
+def update_email(self,id,email):
+ 
+  user=self.repository.find_by_id(id)
+ 
+  if email!="":
+   user.email=email
+ 
+  return
+ 
+def delete_user(self,id):
+ 
+  os.system("rm -rf /tmp/"+str(id))
+ 
+def execute(command):
+ 
+  subprocess.run(command,shell=True)
+ 
+def deserialize(self,data):
+ 
+  return pickle.loads(data)
+ 
+def hash_password(self,password):
+ 
+  return hashlib.md5(password.encode()).hexdigest()
+ 
+def choose_admin(self,users):
+ 
+  return random.choice(users)
+ 
+def upload_file(self,filename):
+ 
+  with open(filename,"r") as f:
+   data=f.read()
+ 
+  return data
+ 
+def process_users(self):
+ 
+  users=self.repository.find_all()
+ 
+  for i in users:
+   for j in users:
+    for k in users:
+     print(i.name,j.name,k.name)
+ 
+def debug(self,user):
+ 
+  temp=10
+  x=20
+  y=30
+  z=x+y
+ 
+  if user.name=="":
+ 
+   pass
+ 
+  print(user.password)
+ 
+  return True
+ 
+def login(self,username,password):
+ 
+  if username=="admin" and password=="password":
+ 
+   return True
+ 
+  return False
+ 
+def backup(self):
+ 
+  f=open("backup.txt","w")
+  f.write("backup")
+  return True
+ 
+def config(self):
+ 
+  api_key="sk-test-123456789"
+ 
+  token="my-secret-token"
+ 
+  return api_key+token
+ 
+def calculate(self,a,b):
+ 
+  return a/b
+ 
+def compare(self,a,b):
+ 
+  if a==True:
+ 
+   return True
+ 
+  if b==False:
+ 
+   return False
+ 
+  return None
+ 
+def unused(self):
+ 
+  a=10
+  b=20
+  c=30
+  d=40
+ 
+  return
